3ea3c133eb93f6f8433fd2adab68d9ccac8d1439
anzharnugraha26 <anzharnugraha50@gmail.com>
2021-04-19 09:18:47 +0700

    update

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -53,31 +53,6 @@ class BlogController extends Controller
         $post->tags()->attach($request->tags);
         return redirect('/admin/blog');
     }
-
-    
-    // public function store(Request $request)
-    // {
-    //     $fileName = '';
-    //     if ($request->image->getClientOriginalName()) {
-    //         $file = str_replace(' ', '', $request->image->getClientOriginalName());
-    //         $fileName = date('mYdHs') . rand(1, 999) . '_' . $file;
-    //         $request->image->move('img/blog', $fileName);
-    //     }
-
-    //     $slug = Str::slug($request->judul);
-    //     $blogs =  Blog::create(array_merge($request->all(), [
-    //         'slug' => $slug,
-    //         'image' => $fileName,
-            
-    //     ]));
-    //     // BlogTag::create([
-    //     //     'blog_id' => $blogs->id,
-    //     //     'tag_blog_id' => array_merge($request->tags)
-    //     // ]);
-        
-    //     return redirect("admin/blog");
-    // }
-
    
     public function findbyCategoryID($id)
     {
